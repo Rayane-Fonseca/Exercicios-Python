bd707d6e6473795108b1d750be08bf304cee46a8
Rayane Porto de Jesus Fonseca <rayane15526@gmail.com>
2025-07-30 14:29:58 -0300

    Add files via upload

---
+++ ex27.py
@@ -0,0 +1,9 @@
+numero1 = 7
+numero2 = 3
+
+def somar(num1, num2):
+    soma = num1 + num2
+    return soma
+
+resultado = somar(numero1, numero2)
+print(f"O resultado da soma dos números é: {resultado}")
---
+++ ex28.py
@@ -0,0 +1,5 @@
+def saudacao(nome, idade):
+    print(f"Olá, {nome}! Você tem {idade} anos")
+
+saudacao("Rayane", 23)
+saudacao("Otávio", 30)
